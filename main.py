55ac4eefaf4825dc3e25f2b3b8c1bf037bc57b84
Majid Alaeinia <MajidAlaeinia@users.noreply.github.com>
2019-05-16 02:37:43 +0430

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ env_file = 'env.txt'
 env_example_file = 'env_example.txt'
 
 if __name__ == '__main__':
-    """
-    Print the uncommon keys in laravel `.env` and `.env.example` files.
-    """
     if len(make_a_list_from_uncommon_items(make_a_list_from_the_file_keys(env_file),
                                            make_a_list_from_the_file_keys(env_example_file))) == 0:
         print('The files have the same keys, no need to double check.')

4ed59fc1393697657d5fabca07e1730d7222fc57
Majid Alaeinia <alaeinia.majid@gmail.com>
2019-05-17 14:41:37 +0430

    Add count_of_keys_available() method usage to the main function

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ env_file = 'env.txt'
 env_example_file = 'env_example.txt'
 
 if __name__ == '__main__':
+    print('----')
+    print('Number of non-blank and non-comment lines in \'' + env_file + '\' is: ' + str(
+        count_of_keys_available(env_file)))
+    print('Number of non-blank and non-comment lines in \'' + env_example_file + '\' is: ' + str(
+        count_of_keys_available(env_example_file)))
+    print('----')
+
     if len(make_a_list_from_uncommon_items(make_a_list_from_the_file_keys(env_file),
                                            make_a_list_from_the_file_keys(env_example_file))) == 0:
         print('The files have the same keys, no need to double check.')
